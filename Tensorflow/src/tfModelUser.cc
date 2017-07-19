59b491866cf226d105e70d6724c201b4f18ab55a
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2017-07-19 14:46:07 +0200

    Renamed class

--- Tensorflow/src/tfModelUser.cc
+++ Tensorflow/src/tfModelUser.cc
@@ -25,9 +25,14 @@
 
 
 
-loadEvalModel::loadEvalModel(std::string modelFileLocation, std::vector<std::string> inputNeurons, std::vector<std::string> outputNeurons)
+tfModelUser::tfModelUser(std::string modelFileLocation, std::vector<std::string> inputNeurons, std::vector<std::string> outputNeurons)
 {
+    tfModelUser(modelFileLocation, inputNeurons.size(), outputNeurons.size());
+    
+}
 
+tfModelUser::tfModelUser(std::string modelFileLocation, unsigned int numberOfInputNeurons, unsigned int numberOfOutputNeurons)
+{
     //
     // object definitions
     //
@@ -38,16 +43,16 @@ loadEvalModel::loadEvalModel(std::string modelFileLocation, std::vector<std::str
 
     // prepare input and output tensors
     // input tensor contains only 1 event with n input variables
-    dnn::tf::Shape xShape[] = { 1, inputNeurons.size() };
+    dnn::tf::Shape xShape[] = { 1, numberOfInputNeurons };
     dnn::tf::Tensor* x = g.defineInput(new dnn::tf::Tensor("input:0", 2, xShape));
     // output tensor can contain either one or more neurons
     dnn::tf::Tensor* y = g.defineOutput(new dnn::tf::Tensor("output:0"));
     // save number of output neurons
-    m_numberOfOutputNeurons =  outputNeurons.size();
+    m_numberOfOutputNeurons =  numberOfOutputNeurons;
 
 }
 
-void loadEvalModel::evalModel(std::vector<float> eventVariables, std::vector<float> &NNOutput)
+void tfModelUser::evalModel(std::vector<float> eventVariables, std::vector<float> &NNOutput)
 {
     // Clear output vector before filling it again
     NNOutput.clear();
@@ -65,7 +70,7 @@ void loadEvalModel::evalModel(std::vector<float> eventVariables, std::vector<flo
 }
 
 
-loadEvalModel::~loadEvalModel()
+tfModelUser::~tfModelUser()
 {
     // cleanup
     // remove tensors manually (you type 'new', you type 'delete')
